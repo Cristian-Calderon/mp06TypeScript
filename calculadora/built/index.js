b98c402544b2cb2827e2afd43b3d5c1a66b0946c
cris-ubuntu <cdcalderonrojas@gmail.com>
2024-10-03 20:16:48 +0200

    Calculadora sencilla terminada

--- calculadora/built/index.js
+++ calculadora/built/index.js
@@ -1,2 +1,27 @@
 "use strict";
-console.log("holaa");
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('.btn');
+let clear = document.getElementById('clear');
+let equalb = document.getElementById('equal');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let btnValue = button.value;
+        display.value = display.value + btnValue;
+    });
+});
+clear.addEventListener('click', () => {
+    display.value = '';
+});
+equalb.addEventListener('click', () => {
+    try {
+        if (display.value.includes('/0')) {
+            display.value = 'Error';
+        }
+        else {
+            display.value = eval(display.value).toString();
+        }
+    }
+    catch (error) {
+        display.value = 'error';
+    }
+});
--- calculadora/src/index.ts
+++ calculadora/src/index.ts
@@ -1,1 +1,27 @@
-console.log("holaa")
+const display = document.getElementById('display') as HTMLInputElement;
+const buttons = document.querySelectorAll('.btn');
+let clear = document.getElementById('clear')!;
+let equalb = document.getElementById('equal')!;
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let btnValue = (button as HTMLInputElement).value;
+        display.value = display.value + btnValue;
+    })
+});
+
+clear.addEventListener('click', () => {
+    display.value = '';
+});
+
+equalb.addEventListener('click', () => {
+    try {
+        if (display.value.includes('/0')) {
+            display.value = 'Error';
+        } else {
+            display.value = eval(display.value).toString();
+        }
+    } catch (error) {
+        display.value = 'error';
+    }
+});
